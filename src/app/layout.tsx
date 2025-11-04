4e629aa0f6898e456324ead0375910f7288037b2
kokekokko0116 <ec0668vx@gmail.com>
2025-11-04 16:32:23 +0900

    直接headタグに

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,6 @@ export const metadata: Metadata = {
   title: "タケヤ電機株式会社 | 運命の１台 届けます",
   description: "創業70周年。香川県で愛される地域密着型の家電販売・リフォーム事業を展開。家電+リフォーム＝２倍の幸せをお届けします。",
   keywords: "タケヤ電機,家電,リフォーム,香川県,高松市,屋島,扇町,志度,三木",
-  verification: {
-    google: "D-OuhhbGHZd3mzXG7RIiT3HxdsIICdvAStyMHrcmq6c",
-  },
 };
 
 export default function RootLayout({
@@ -18,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <meta name="google-site-verification" content="D-OuhhbGHZd3mzXG7RIiT3HxdsIICdvAStyMHrcmq6c" />
+      </head>
       <body>
         <ThemeRegistry>
           {children}
